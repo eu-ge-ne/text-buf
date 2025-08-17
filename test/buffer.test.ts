e4fbea50eeb51453f26c2090e9b1500bc0dd01c7
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-08-17 19:43:42 -0400

    1

--- test/buffer.test.ts
+++ test/buffer.test.ts
@@ -56,35 +56,21 @@ Deno.test("find_eol_index", () => {
   assertEquals(buf.find_eol_index(8, 0), 2);
 });
 
-Deno.test("slice_eols", () => {
+Deno.test("find_eol_index_2", () => {
   const buf = new Buffer("1\n2\n3\n4\n5");
   //                      01 23 45 67 8
   //                       0  1  2  3
 
   assertEquals(buf.eols_len, 4);
 
-  assertEquals(buf.slice_eols(0, 9), [0, 4]);
-
-  assertEquals(buf.slice_eols(1, 9), [0, 4]);
-  assertEquals(buf.slice_eols(2, 9), [1, 4]);
-  assertEquals(buf.slice_eols(3, 9), [1, 4]);
-  assertEquals(buf.slice_eols(4, 9), [2, 4]);
-  assertEquals(buf.slice_eols(5, 9), [2, 4]);
-  assertEquals(buf.slice_eols(6, 9), [3, 4]);
-  assertEquals(buf.slice_eols(7, 9), [3, 4]);
-  assertEquals(buf.slice_eols(8, 9), [4, 4]);
-
-  assertEquals(buf.slice_eols(0, 8), [0, 4]);
-  assertEquals(buf.slice_eols(0, 7), [0, 3]);
-  assertEquals(buf.slice_eols(0, 6), [0, 3]);
-  assertEquals(buf.slice_eols(0, 5), [0, 2]);
-  assertEquals(buf.slice_eols(0, 4), [0, 2]);
-  assertEquals(buf.slice_eols(0, 3), [0, 1]);
-  assertEquals(buf.slice_eols(0, 2), [0, 1]);
-  assertEquals(buf.slice_eols(0, 1), [0, 0]);
-
-  assertEquals(buf.slice_eols(1, 8), [0, 4]);
-  assertEquals(buf.slice_eols(2, 7), [1, 3]);
-  assertEquals(buf.slice_eols(3, 6), [1, 3]);
-  assertEquals(buf.slice_eols(4, 5), [2, 2]);
+  assertEquals(buf.find_eol_index(0, 0), 0);
+  assertEquals(buf.find_eol_index(1, 0), 0);
+  assertEquals(buf.find_eol_index(2, 0), 1);
+  assertEquals(buf.find_eol_index(3, 0), 1);
+  assertEquals(buf.find_eol_index(4, 0), 2);
+  assertEquals(buf.find_eol_index(5, 0), 2);
+  assertEquals(buf.find_eol_index(6, 0), 3);
+  assertEquals(buf.find_eol_index(7, 0), 3);
+  assertEquals(buf.find_eol_index(8, 0), 4);
+  assertEquals(buf.find_eol_index(8, 0), 4);
 });
